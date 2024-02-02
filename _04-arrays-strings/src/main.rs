4a5a543b3239e160dba27e7cf6d67961a6725fc3
ktkafy <redteamsec2022@gmail.com>
2024-02-03 00:28:32 +0330

    vector

--- _04-arrays-strings/src/main.rs
+++ _04-arrays-strings/src/main.rs
@@ -1,8 +1,31 @@
 fn main() {
-    let l:[i32;7] = [4,5,6,7,8,9,1];
-    println!("{:#?}",l.as_ptr());
-    unsafe{
-    let temp: u8 = std::ptr::read((l.as_ptr() as isize + (std::mem::size_of::<i32>()*3) as isize) as *const u8);
-    print!("{}",temp);
-   }
+    let mut l:[i32;100] = [0;100];
+    let mut v1:Vec<i32> = vec![2,3,6,8,9];
+    v1.push(100);
+    let temp = v1.pop().unwrap();
+    println!("{}", temp);
+    println!("===============");
+    println!("{:#?}", v1);
+    println!("===============");
+    let v2:Vec<i32> = Vec::new();
+    
+    let t:i32 = v1.iter().sum();
+    println!("sum is: {}", t);
+
+    for i in v1.clone().into_iter(){
+        //*i +=100;
+        println!("{}", i);
+    }
+    println!("=======after iterate========");
+    println!("{:?}", v1);
+    /* 
+    let t = l.iter().map(|x| x + 200).collect::<Vec<i32>>();
+    println!("{:#?}",t);
+
+    for i in l.into_iter(){
+        println!("{}", i);
+    }
+    println!("{:?}", l.contains(&8));
+    */
 }
+
